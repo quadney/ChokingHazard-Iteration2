ac4b43bf0f686227bcec1eb6e31f65587524fcc9
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2014-03-01 23:09:38 -0500

    after 1.5 years of personal agony with reading images, I have finally figured out reading images. That is my commit.

--- src/Views/HelpFrame.java
+++ src/Views/HelpFrame.java
@@ -1,7 +1,42 @@
 package Views;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class HelpFrame extends JFrame {
-
+	private final int WIDTH, HEIGHT;
+	BufferedImage helpImage;
+	
+	public HelpFrame(){
+		WIDTH = 915;
+		HEIGHT = 450;
+		setTitle("Keyboard Controls");
+		setSize(WIDTH, HEIGHT);
+		setResizable(false);
+		loadPanel();
+	}
+	
+	private void loadPanel(){
+		try{
+			helpImage = ImageIO.read(this.getClass().getResource("/images/helpControls.png"));
+			displayBufferedImage(helpImage);
+		}catch(IOException e){
+				System.out.println(e);
+		}
+		
+	}
+	
+	private void displayBufferedImage(BufferedImage image){
+		this.setContentPane(new JScrollPane(new JLabel(new ImageIcon(image))));
+		this.validate();
+	}
 }
