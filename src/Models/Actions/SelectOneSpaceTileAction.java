a47067b222a8fa6fd13e29f2611839afa54799f2
Meghan King <meghantking67@gmail.com>
2014-03-15 18:26:49 -0400

    Updated and Extended MAction hierarchy for tile actions

--- src/Models/Actions/SelectOneSpaceTileAction.java
+++ src/Models/Actions/SelectOneSpaceTileAction.java
@@ -1,26 +1,23 @@
 package Models.Actions;
 
-public class SelectTileAction extends MAction {
+public class SelectOneSpaceTileAction extends MAction {
 
-	int rotationState = 0;
 	int x = 1;
 	int y = 1;
 	
-	public SelectTileAction(int playerIndex, int x, int y, int rotationState) {
+	public SelectOneSpaceTileAction(int playerIndex, int x, int y) {
 		super(playerIndex);
+		
 		// TODO Auto-generated constructor stub
 		
 	}
 
 	public void pressSpace(){
-		rotationState += 1 % 4;
+		//does nothing
 	}
 	
 	public void pressCursor(){
 		
 	}
-	
-	
-	
 	
 }
