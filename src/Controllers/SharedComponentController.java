d99c79fab5cfbd653a1e1a561cb3b70db6a28381
Meghan King <meghantking67@gmail.com>
2014-03-15 15:48:39 -0400

    Implemented select 3 tile action with SharedComponentController

--- src/Controllers/SharedComponentController.java
+++ src/Controllers/SharedComponentController.java
@@ -14,4 +14,29 @@ public class SharedComponentController {
 	public SharedComponentPanel getSharedComponentPanel(){
 		return this.sharedPanel;
 	}
+	
+	//called if the player has selected a three piece tile.
+	//checks to see if there enough three piece tiles to allow this to happen
+	//if it does, it changes the value in the mode and tells the panel to update
+	public boolean useThreeTile(){
+		if(sharedModel.useThreeTile()){
+			sharedPanel.updateThreePieceTiles(sharedModel.getThreeSpaceTiles());
+			return true;
+		}
+		return false;
+	}
+	
+	//called if the player has pressed escape while having a three piece tile selected
+	//tells the model to change to number of three tiles
+	//tells the panel to update
+	public void putBackThreeTile(){
+		sharedModel.incrementThreeTile();
+		sharedPanel.updateThreePieceTiles(sharedModel.getThreeSpaceTiles());
+	}
+	
+	
+	
+	
+	
+	
 }
