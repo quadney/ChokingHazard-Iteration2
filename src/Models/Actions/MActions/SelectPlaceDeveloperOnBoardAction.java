8723eb368f60d361e3e80ddd770d91b64f4cda5c
Meghan King <meghantking67@gmail.com>
2014-03-17 17:51:50 -0400

    Renamed some of MAction hierarchy to be more exact. Starting to create a developer actions

---
+++ src/Models/Actions/MActions/SelectPlaceDeveloperOnBoardAction.java
@@ -0,0 +1,12 @@
+package Models.Actions.MActions;
+
+public class SelectPlaceDeveloperOnBoardAction extends SelectOneSpaceComponentAction {
+
+	public SelectPlaceDeveloperOnBoardAction(String imageKey) {
+		super(imageKey);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
