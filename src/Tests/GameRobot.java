163a1884fcd396913f15ef023693b057f47cf75b
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-03-17 20:37:20 -0400

    Comments for eclipse calls in GameRobot

--- src/Tests/GameRobot.java
+++ src/Tests/GameRobot.java
@@ -17,15 +17,30 @@ public class GameRobot {
 	     robot = tmp;
 	   }
 
-	public static void click(int x, int y, int times) {
+	/**
+	 * clicks on pixel x, y. Does this n times if specified.
+	 */
+	public static void click(int x, int y, int n) {
 		robot.mouseMove(x, y);
 		throttle();
-		for(int i = 0; i < times; ++i) {
+		for(int i = 0; i < n; ++i) {
 			robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
 			robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 		}
 	}
+	
+	/**
+	 * clicks on pixel x, y once.
+	 */
+	public static void click(int x, int y) {
+		click(x, y, 1);
+	}
 
+
+	/**
+	 * "drags" the mouse from spot startX, startY to endX, endY; in pixels.
+	 * Clicks on start coordinates, releases at end coordinates.
+	 */
 	public static void drag(int startX, int startY, int endX, int endY) {
 		robot.mouseMove(startX, startY);
 		robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
@@ -33,6 +48,9 @@ public class GameRobot {
 		robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 	}
 	
+	/**
+	 * moves the mouse to pixel x, y. Doesn't click.
+	 */
 	public static void navigateCursor(int x, int y) {
 		int horizontal = x < 0 ? KeyEvent.VK_LEFT : KeyEvent.VK_RIGHT;
 		int vertical = y < 0 ? KeyEvent.VK_UP : KeyEvent.VK_DOWN;
@@ -46,24 +64,36 @@ public class GameRobot {
 		}
 	}
 	
+	/**
+	 * mimics the press and release of the spacebar.
+	 */
 	public static void pressSpace() {
 		type(" ");
 	}
 	
+	/**
+	 * mimics keyboard combination alt+N. 
+	 */
 	public static void altN() {
 		press(KeyEvent.VK_ALT);
 		for(int x = 0; x < 1; ++x) 
 			type("n");
 		release(KeyEvent.VK_ALT);
 	}
-	
+
+	/**
+	 * mimics keyboard combination alt+O. 
+	 */
 	public static void altO() {
 		press(KeyEvent.VK_ALT);
 		for(int x = 0; x < 1; ++x) 
 			type("o");
 		release(KeyEvent.VK_ALT);
 	}
-	
+
+	/**
+	 * mimics keyboard combination alt+Q. 
+	 */
 	public static void altQ() {
 		press(KeyEvent.VK_ALT);
 		for(int x = 0; x < 1; ++x) 
@@ -71,33 +101,51 @@ public class GameRobot {
 		release(KeyEvent.VK_ALT);
 	}
 	
+	/**
+	 * mimics press and release of enter key
+	 */
 	public static void pressEnter() {
 		press(KeyEvent.VK_ENTER);
 		release(KeyEvent.VK_ENTER);
 	}
 
+	/**
+	 * mimics press and release of escape key
+	 */
 	public static void pressEscape() {
 		press(KeyEvent.VK_ESCAPE);
 		release(KeyEvent.VK_ESCAPE);
 	}
-	
+
+	/**
+	 * mimics press and release of tab key
+	 */
 	public static void pressTab() {
 		press(KeyEvent.VK_TAB);
 		release(KeyEvent.VK_TAB);
 	}
-	
+
+	/**
+	 * mimics press and release of delete key
+	 */
 	public static void pressDelete() {
 		press(KeyEvent.VK_DELETE);
 		release(KeyEvent.VK_DELETE);
 	}
-	
+
+	/**
+	 * mimics press and release of backspace key
+	 */
 	public static void pressBackspace() {
 		press(KeyEvent.VK_BACK_SPACE);
 		release(KeyEvent.VK_BACK_SPACE);
 	}
-	
-	public static void pressTab(int times) {
-		for(int x = 0; x < times; ++x)
+
+	/**
+	 * mimics press and release of the tab key n times
+	 */
+	public static void pressTab(int n) {
+		for(int x = 0; x < n; ++x)
 			pressTab();
 	}
 	
@@ -124,6 +172,10 @@ public class GameRobot {
 		}
 	}
 	
+	/**
+	 * pauses simulator for an amount of milliseconds
+	 * @param time, milliseconds that the simulator Thread pauses for.
+	 */
 	public static void wait(int time) {
 		try {
 			Thread.sleep(time);
@@ -133,6 +185,9 @@ public class GameRobot {
 		}
 	}
 	
+	/**
+	 * private method to pause for a constant amount of time between key presses.
+	 */
 	private static void throttle() {
 		try {
 			Thread.sleep(Simulator.THROTTLE);
@@ -142,11 +197,19 @@ public class GameRobot {
 		}
 	}
 	
+	/**
+	 * private method to press a keycode and then throttle
+	 * @param keycode
+	 */
 	private static void press(int keycode) {
 		robot.keyPress(keycode);
 		throttle();
 	}
-	
+
+	/**
+	 * private method to release a keycode and then throttle
+	 * @param keycode
+	 */
 	private static void release(int keycode) {
 		robot.keyRelease(keycode);
 		throttle();
--- src/Tests/Simulator.java
+++ src/Tests/Simulator.java
@@ -13,7 +13,7 @@ public class Simulator {
 	public static int QUIT_POS_Y = 140;
 	public static int PLACE_START_X = 1;
 	public static int PLACE_START_Y = 1;
-	public static int THROTTLE = 100;
+	public static int THROTTLE = 30;
 	public static int TIME_BETWEEN_MAIN_PLAYERS = 150;
 	public static int TIME_BETWEEN_MAIN_COMMANDS = 100;
 	public static int GAME_LOAD_WAIT_TIME = 2000;
