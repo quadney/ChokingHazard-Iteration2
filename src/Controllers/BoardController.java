23a5876676e160a14a8d84b17c82a7df07a7105f
Meghan King <meghantking67@gmail.com>
2014-03-18 21:47:15 -0400

    Took out unneeded if statements in GCtrl key press actions

--- src/Controllers/BoardController.java
+++ src/Controllers/BoardController.java
@@ -2,7 +2,7 @@ package Controllers;
 
 import Models.BoardModel;
 import Models.Actions.Action;
-import Models.Actions.NonRotatableComponentAction;
+import Models.Actions.OneSpaceTileAction;
 import Models.Actions.RotatableComponentAction;
 import Views.BoardPanel;
 
@@ -36,11 +36,9 @@ public class BoardController {
 			System.out.println("in updateBoardPanel RotatableTileComponent");
 			boardPanel.placeTile(((RotatableComponentAction)action).getY()*50, ((RotatableComponentAction)action).getX()*50, ((RotatableComponentAction)action).getRotationState(), 1, action.imageKey);
 		}
-		else if(action instanceof NonRotatableComponentAction){
+		else if(action instanceof OneSpaceTileAction){
 			System.out.println("in updateBoardPanel NonRotatableTileComponent");
-			boardPanel.placeTile(((NonRotatableComponentAction)action).getY()*50, ((NonRotatableComponentAction)action).getX()*50, 0, 1, action.imageKey);
+			boardPanel.placeTile(((OneSpaceTileAction)action).getY()*50, ((OneSpaceTileAction)action).getX()*50, 0, 1, action.imageKey);
 		}
-		
-		
 	}
 }
