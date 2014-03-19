8cfe6b40a29d5fba24d5fe46273a4b94794fa5b4
Meghan King <meghantking67@gmail.com>
2014-03-18 21:05:18 -0400

    Commented out print statement in BoardModel when the Java Cells are instantiated

--- src/Controllers/BoardController.java
+++ src/Controllers/BoardController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Models.BoardModel;
 import Models.Actions.Action;
+import Models.Actions.NonRotatableComponentAction;
 import Models.Actions.RotatableComponentAction;
 import Views.BoardPanel;
 
@@ -34,6 +35,10 @@ public class BoardController {
 		if(action instanceof RotatableComponentAction){
 			System.out.println("in updateBoardPanel RotatableTileComponent");
 			boardPanel.placeTile(((RotatableComponentAction)action).getY()*50, ((RotatableComponentAction)action).getX()*50, ((RotatableComponentAction)action).getRotationState(), 1, action.imageKey);
+		}
+		else if(action instanceof NonRotatableComponentAction){
+			System.out.println("in updateBoardPanel NonRotatableTileComponent");
+			boardPanel.placeTile(((NonRotatableComponentAction)action).getY()*50, ((NonRotatableComponentAction)action).getX()*50, 0, 1, action.imageKey);
 		}
 		
 		
